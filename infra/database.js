f0b5a316aeb6d171596884db8ed0b27d3fdfd430
Mateus Araújo <mathsilvaa20@hotmail.com>
2024-12-12 04:29:24 +0000

    add files  and

---
+++ infra/database.js
@@ -0,0 +1,19 @@
+import { Client } from "pg";
+
+async function query(queryObject) {
+  const client = new Client({
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    database: process.env.POSTGRESS_DATABASE,
+    user: process.env.POSTGRESS_USER,
+    password: process.env.POSTGRESS_PASSWORD,
+  });
+  await client.connect();
+  const result = await client.query(queryObject);
+  await client.end();
+  return result;
+}
+
+export default {
+  query: query,
+};
--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,5 +1,8 @@
-function status(request, response) {
-  response.status(200).json({ success: "ok" });
+import database from "../../../../infra/database";
+
+async function status(request, response) {
+  const result = await database.query("SELECT 1 + 1 as sum;");
+  response.status(200).json({ success: result.rows });
 }
 
 export default status;
